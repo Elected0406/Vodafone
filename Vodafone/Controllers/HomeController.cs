63e8ae4b9acf2619cc457b46661f7d8721fa2a91
Elected0406 <vl.com.md2@mail.ru>
2017-02-24 14:51:01 +0200

    допилен конфиг

--- Vodafone/Controllers/HomeController.cs
+++ Vodafone/Controllers/HomeController.cs
@@ -9,18 +9,30 @@ namespace Vodafone.Controllers
 {
     public class HomeController : Controller
     {
+        Userdatabase db = new Userdatabase();
+
         [HttpGet]
         public ActionResult Index(Keyfromlink keyfromlink)
         {
-            if (ModelState.IsValid) {
-                if (ValidateUserKeys(Model.Ids))
+            //var Keys = from p in Userdatabase.Keys select p;
+            //model.Persons = persons.ToList();
+            if (keyfromlink.Atribute.Equals(0))
+            {
+                ViewBag.Hepuhv = "Hepuhv0";
+                ViewBag.Mepuhv = "Mepuhv0";
+                ViewBag.Lepuhv = "Lepuhv0";
+                ViewBag.Ulepuhv = "Ulepuhv0";
+                ViewBag.Lpdrp = "Lpdrp0";
             }
-            IEnumerable<UserKeys> keyfromlink.Atribute
-            ViewBag.Hepuhv = keyfromlink.Atribute;
-            ViewBag.Mepuhv = "Mepuhv";
-            ViewBag.Lepuhv = "Lepuhv";
-            ViewBag.Ulepuhv = "Ulepuhv";
-            ViewBag.Lpdrp = "Lpdrp";
+
+            if (keyfromlink.Atribute.Equals(101))
+            {
+                ViewBag.Hepuhv = "Hepuhv101";
+                ViewBag.Mepuhv = "Mepuhv101";
+                ViewBag.Lepuhv = "Lepuhv101";
+                ViewBag.Ulepuhv = "Ulepuhv101";
+                ViewBag.Lpdrp = "Lpdrp101";
+            }
             return View();
         }
         public ActionResult About()
